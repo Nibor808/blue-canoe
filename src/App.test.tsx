8e6f90cac0a26cd1923e0e846b47eec85d93913b
Robin Erickson <re@incuboweb.ca>
2022-09-17 08:10:00 -0400

    test space connection

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 describe('<App />', () => {
-    it('should render the Header', () => {
+    it('should render the Header and an image', () => {
         render(<App />);
         const headerText = screen.getByText(/The Blue Canoe/i);
         const subText = screen.getByText(/Adventures in the Kawarthas/i);
+        const image = screen.getByTestId('us-img');
         expect(headerText).toBeInTheDocument();
         expect(subText).toBeInTheDocument();
-    });
-
-    it('should render the image', () => {
-        render(<App />);
-        const image = screen.getByTestId('us-img');
         expect(image).toBeInTheDocument();
     });
 });
